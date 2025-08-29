5859e377e981a7d8181b2881c8be4e5ec8bad526
Masuduzzaman <md.santo0583210@gmail.com>
2025-08-29 16:47:44 +0600

    first commit

---
+++ home.js
@@ -0,0 +1,75 @@
+// Navbar Counters
+let heartCount = 0;
+let coinCount = 100;
+let copyCount = 0;
+
+const heart = document.getElementById("heart");
+const coin = document.getElementById("coinCount");
+const copy = document.getElementById("nav-copy");
+
+
+// Heart Button Function
+const heartButtons = document.querySelectorAll("[id^='heart-btn-']");
+heartButtons.forEach(btn => {
+    btn.addEventListener("click", function() {
+        heartCount++;
+        heart.textContent = heartCount;
+    });
+});
+
+// Copy Button Function
+const copyButtons = document.querySelectorAll(".fa-copy").forEach(copyIcon => {
+    const btn = copyIcon.parentElement;
+    btn.addEventListener("click", () => {
+        const card = btn.closest("div.col-span-3");
+        const number = card.querySelector("h1").innerText;
+
+        navigator.clipboard.writeText(number).then(() => {
+            alert(`Copied number: ${number}`);
+            copyCount++;
+            copy.textContent = copyCount;
+        });
+    });
+});
+
+// Call Button Function
+
+const callButtons = document.querySelectorAll(".fa-phone").forEach(callIcon => {
+    const btn = callIcon.parentElement;
+    btn.addEventListener("click", () => {
+        const card = btn.closest("div.col-span-3");
+        const serviceName = card.querySelector("h2").innerText;
+        const number = card.querySelector("h1").innerText;
+
+        if (coinCount < 20) {
+            alert("Not enough coins");
+            return;
+        }
+
+        coinCount -= 20;
+        coin.textContent = coinCount;
+
+        const time = new Date().toLocaleTimeString();
+
+        alert(`Calling ${serviceName} at ${number}`);
+
+        // Add to history
+        const historyDiv = document.getElementById("history");
+        const item = document.createElement("div",);
+        item.classList.add("py-2","px-2",);
+        item.innerHTML = `
+            <div class="flex justify-between items-center ">
+            <div>
+            <p class="font-bold">${serviceName}</p>
+            <p>${number}</p>
+            </div>
+            <p class="text-sm text-black">${time}</p></div>
+        `;
+        historyDiv.appendChild(item);
+    });
+});
+
+// Clear History
+document.getElementById("clear-btn").addEventListener("click",function() {
+    document.getElementById("history").innerHTML = "";
+});
